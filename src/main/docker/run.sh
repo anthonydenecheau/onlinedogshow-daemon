e60788f64ae517848c7ee6643bf0a93d187ca14a
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2018-02-07 15:59:26 +0100

    run.sh

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -34,4 +34,4 @@ java -Djava.security.egd=file:/dev/./urandom -Dserver.port=$SERVER_PORT   \
      -Dspring.cloud.stream.kafka.binder.zkNodes=$KAFKASERVER_URI          \
      -Dspring.cloud.stream.kafka.binder.brokers=$ZKSERVER_URI             \
      -Dspring.zipkin.baseUrl=$ZIPKIN_URI                                  \
-     -Dspring.profiles.active=$PROFILE -jar /usr/local/agriaservice/@project.build.finalName@.jar
+     -Dspring.profiles.active=$PROFILE -jar /usr/local/daemonservice/@project.build.finalName@.jar
