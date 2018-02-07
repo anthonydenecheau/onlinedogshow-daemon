cfab465a78f2ccf8ebf883bd462c7e23c08dbcc0
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2018-02-07 15:14:53 +0100

    run.sh

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -3,7 +3,7 @@
 echo "********************************************************"
 echo "Waiting for the database server to start on port $DATABASESERVER_PORT"
 echo "********************************************************"
-while ! `nc -z database $DATABASESERVER_PORT`; do sleep 3; done
+while ! `nc -z $DATABASESERVER_NAME $DATABASESERVER_PORT`; do sleep 3; done
 echo "******** Database Server has started "
 
 echo "********************************************************"
